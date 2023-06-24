887054fdb84927457afea73111dffe062835aa47
Brick Reyes <brickrey@hotmail.com>
2023-06-24 10:35:31 -0500

    Set up singleton class

--- YugiohCard/app/src/main/java/com/brickrey/yugiohcard/GlobalData.java
+++ YugiohCard/app/src/main/java/com/brickrey/yugiohcard/GlobalData.java
@@ -1,4 +1,13 @@
 package com.brickrey.yugiohcard;
 
 public class GlobalData {
+
+    //<editor-fold defaultState="collapsed" desc="DriverApp Global Data Implementation"
+    private static final GlobalData instance = new GlobalData();
+
+    public static GlobalData getInstance() {
+        return instance;
+    }
+    //</editor-fold>
+
 }
